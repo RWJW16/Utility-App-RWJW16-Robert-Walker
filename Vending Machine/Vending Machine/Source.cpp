ff0009e1a394159cd20ee0648882a5acbfa9f006
RWJW16 <72308929+RWJW16@users.noreply.github.com>
2021-01-28 16:38:49 +0000

    issues resolved, stock system added

--- Vending Machine/Vending Machine/Source.cpp
+++ Vending Machine/Vending Machine/Source.cpp
@@ -2,61 +2,125 @@
 #include <string>
 using namespace std;
 
-int selection(string action, double input, int max, int hoc);
-void newItem(int hotorcold);
-
-string drinks[2][5] = {
-		{"Coke ", "Dr Pepper ", "Pepsi", "Sprite ", "Fanta"},
-		{"Coffee ", "Tea ", "Hot Chocolate ", "Horlicks ", "Green tea "}
-};
-
-int Prices[2][5] = {
-	{1.50 /* Coke */, 1.20 /* Dr Pepper */, 1.80 /* Pepsi */, 1.00 /*Sprite */, 1.50 /* Fanta */},
-	{1.20 /* Coffee */, 1.00 /* Tea */, 1.10 /* Hot Chocolate */, 1.50 /* Horlicks */, 2.00 /* Green tea */}
-};
-
-string menu;
+int selection(string action, double input, int max);
 
 int main() {
+
+	int countH = 6;
+	int countC = 6;
+
+	string drinks[2][10] = {
+		{"Coke ", "Dr Pepper ", "Pepsi ", "Sprite ", "Fanta "},
+		{"Coffee ", "Tea ", "Hot Chocolate ", "Horlicks ", "Green tea "}
+	};
+
+	double Prices[2][10] = {
+		{1.50 , 1.20 , 1.80 , 1.00 , 1.50 },
+		{1.20 , 1.00 , 1.10 , 1.50 , 2.00 }
+	};
+
+	int stock[2][10] = {
+		{5 , 4 , 0 , 7, 1 },
+		{5 , 6 , 1 , 0 , 2 }
+	};
 
 	char again;
 	double change = 0;
 	do {
-		change = selection("Insert Change", 0, 100, 0);
+		change = selection("Insert Change", 0, 100);
 		change = change / 100;
 
-		cout << "You have inserted: " << change << endl;
-		cout << endl;
+		cout << "You have inserted: " << change << endl << endl;
 
-
+		
 		while (true) {
-			int hoc = selection("1.Cold drinks\n2.Hot drinks", 0, 2, 0);
+			cout << "would you like a hot or cold drink?" << endl;
+			int hoc = selection("1.Cold drinks\n2.Hot drinks", 0, 2);
 			hoc = hoc / 100;
 			hoc--;
 			cout << endl;
 
-			
+			cout << "Please select a drink (type 12 to add a new drink to the menu)" << endl;
+			string menu = " ";
+			int num = 1;
 			if (hoc == 0) {
-				menu = "1.Coke 1.50\n2.Dr Pepper 1.20\n3.Pepsi 1.80\n4.Sprite 1.00\n5.Fanta 1.50\n";
+				for (int i = 0; i < 10; i++) {
+					menu = menu + to_string(num) + "." + drinks[hoc][i] + " " + to_string(Prices[hoc][i]) + "\n";
+					num++;
+				}
 			}
 			else {
-				menu = "1.Coffee 1.20\n2.Tea 1.00\n3.Hot Chocolate 1.10\n4.Horlicks 1.50\n5.Green tea 2.00\n";
+				for (int i = 0; i < 10; i++) {
+					menu = menu  + to_string(num) + "." + drinks[hoc][i] + " " + to_string(Prices[hoc][i]) + "\n";
+					num++;
+				}
 			}
 
-			int which = selection(menu, 0, 5, hoc);
+			int which = selection(menu, 0, 12);
 			which = which / 100;
 			which--;
 
-			if (which == 101) {
-				cout << "New item added" << endl;
+			if (which == 11) {
+				int count = 0;
+				string item;
+				double price = 0;
+
+				switch (hoc) {
+				case 0:
+					count = countC;
+					break;
+
+				case 1:
+					count = countH;
+					break;
+
+				default:
+					cout << "Incorrect selection" << endl;
+					break;
+
+				}
+
+				cout << "\nAdding new item to selected category" << endl;
+
+				cout << "What is the name of the item?" << endl;
+				cin >> item;
+
+				cout << "How much will it cost?" << endl;
+				cin >> price;
+
+				drinks[hoc][(count - 1)] = item;
+				Prices[hoc][(count - 1)] = price;
+				stock[hoc][(count - 1)] = 10;
+
+
+				count++;
+				switch (hoc) {
+				case 0:
+					countC = count;
+					break;
+
+				case 1:
+					countH = count;
+					break;
+
+				default:
+					cout << "Incorrect selection" << endl;
+					break;
+
+				}
+				cout << endl;
+			}
+			else if (stock[hoc][which] == 0) {
+				cout << "This drink is currently out of stock, you will be refunded " << change << endl << "Please select another drink" << endl << endl;
 			}
 			else if (change - Prices[hoc][which] < 0) {
 				cout << "You do not have enough money for this drink, please select another" << endl;
 			}
 			else {
 				change = change - Prices[hoc][which];
-				cout << drinks[hoc][which] << "Vended" << endl << endl;
+				cout << endl << drinks[hoc][which] << "Vended" << endl;
 				cout << change << " pounds returned as change" << endl;
+				stock[hoc][which] = stock[hoc][which] - 1;
 				break;
 			}
 		}
@@ -67,73 +131,21 @@ int main() {
 	return 0;
 }
 
-int selection(string action, double input, int max, int hoc) {
+int selection(string action, double input, int max) {
 
 	cout << action << endl;
 	cin >> input;
 
-	if (max == 5 && input == 79.62) {
-		newItem(hoc);
-	}
-	else {
-		while (cin.fail() || input < 1 || input > max) {
-			cout << "Invalid input" << endl;
-			cin.clear();
-			cin.ignore(1000, '\n');
+	while (cin.fail() || input < 1 || input > max) {
+		cout << "Invalid input" << endl;
+		cin.clear();
+		cin.ignore(1000, '\n');
 
-			cout << action << endl;
-			cin >> input;
-		}
+		cout << action << endl;
+		cin >> input;
 	}
-
+	
 	input = input * 100;
 	return input;
 }
 
-void newItem(int hotorcold) {
-	int countH = 6;
-	int countC = 6;
-	int count = 0;
-	string item;
-	int price = 0;
-
-	while (hotorcold > 1) {
-		switch (hotorcold) {
-			case 0:
-				count = countC;
-				break;
-
-			case 1:
-				count = countH;
-				break;
-
-			default:
-				cout << "Incorrect selection" << endl;
-				break;
-
-		}
-	}
-
-	cout << "\nAdding new item to selected category" << endl;
-
-	cout << "What is the name of the item?" << endl;
-	cin >> item;
-
-	cout << "How much will it cost?" << endl;
-	cin >> price;
-
-	drinks[hotorcold][(count - 1)] = item;
-	Prices[hotorcold][(count - 1)] = price;
-
-	string FItem = to_string(count) + "." + item + to_string(price) + "\n";
-
-	menu = menu + FItem;
-
-	count++;
-	if (hotorcold == 0) {
-		countC = count;
-	}
-	else {
-		countH = count;
-	}
-}
